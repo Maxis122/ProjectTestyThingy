6d3a36712b221bbd366d359b8b570b91e6901bf4
Maxis122 <maxis122@hotmail.co.uk>
2016-10-10 20:36:04 +0100

    More Fun Stuff

---
+++ ProjectTestyThingy/Assets/Scripts/movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int speed = 5;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
