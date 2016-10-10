44931dd58eeaf4300f5b827507657eaac3aad465
Maxis122 <maxis122@hotmail.co.uk>
2016-10-10 20:57:03 +0100

    Added Movement Script
    Left and right doesn't work yet.

--- ProjectTestyThingy/Assets/Scripts/movement.cs
+++ ProjectTestyThingy/Assets/Scripts/movement.cs
@@ -2,15 +2,32 @@
 using System.Collections;
 
 public class movement : MonoBehaviour {
+	public Vector3 vspeed = new Vector3(0, 0, 5);
+	public Vector3 hspeed = new Vector3(5, 0, 0);
+	private Vector3 xmovement;
+	private Vector3 ymovement;
 
 	// Use this for initialization
 	void Start () {
-		int speed = 5;
-
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetAxis ("Horizontal") != 0) {
+			xmovement = new Vector3 (hspeed.x, hspeed.y, hspeed.z);
+			xmovement *= Input.GetAxis ("Horizontal");
+
+		}
+
+		if (Input.GetAxis ("Vertical") != 0) {
+			ymovement = new Vector3 (vspeed.x, vspeed.y, vspeed.z);
+			ymovement *= Input.GetAxis ("Vertical");
+		}
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody>().velocity = xmovement + ymovement;
 	}
 }
